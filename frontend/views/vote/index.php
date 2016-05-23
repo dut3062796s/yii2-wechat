0a05f696733c78cf1c385c7e5e53a5444daa88db
刘俊涛 <liujuntaor@qq.com>
2016-05-23 18:31:34 +0800

    new

--- frontend/views/vote/index.php
+++ frontend/views/vote/index.php
@@ -5,7 +5,9 @@
  * Date: 16/5/23
  * Time: 下午4:59
  */
-$this->title = '投票';
+$this->title = '参赛人员列表';
+$this->params['breadcrumbs'][] = ['label' => '参加大赛', 'url' => ['create']];
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 <?= \yii\widgets\ListView::widget([
     'dataProvider' => $dataProvider,
