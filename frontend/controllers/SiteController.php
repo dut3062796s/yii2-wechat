02db841a89892e1e9ad640e6d06cc1941bd820e1
刘俊涛 <liujuntaor@qq.com>
2016-05-24 14:44:25 +0800

    vote

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -39,7 +39,7 @@ class SiteController extends Controller
             if (!empty($vote)) {
                 $vote->num += 1;
                 $vote->save();
-                $msg = '成功为%d号选手投票,该选手现在票数为%s,当前排在第%d名!详情:%s';
+                $msg = '成功为%s号选手投票,该选手现在票数为%s,当前排在第%s名!详情:%s';
                 $rank = VoteUser::find()->where(['>', 'num', $vote->num])->count() + 1;
                 return $this->renderText(printf($msg, $vote->id, $vote->num, $rank, Url::to(['/vote/info', 'id' => 1], true)));
             }
