26f29a5b16dfb47a78bb716d622e59c3ea3ccc7f
刘俊涛 <liujuntaor@qq.com>
2016-05-24 17:10:39 +0800

    vote

--- frontend/views/vote/view.php
+++ frontend/views/vote/view.php
@@ -9,4 +9,11 @@
 
 <h1><?= $model->name ?></h1>
 <div><?= \common\helpers\Html::img($model->cover) ?></div>
-<div><?= \yii\helpers\HtmlPurifier::process($model->description) ?></div>
+<div class="panel panel-default">
+    <div class="panel-header">
+        <h2>吹牛宣言</h2>
+    </div>
+    <div class="panel-body">
+        <?= \yii\helpers\HtmlPurifier::process($model->description) ?>
+    </div>
+</div>
