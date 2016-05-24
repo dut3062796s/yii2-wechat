4888f7623c11ebf82225c9a5fa568d0d5df98e4e
刘俊涛 <liujuntaor@qq.com>
2016-05-24 17:22:27 +0800

    vote

--- frontend/views/vote/view.php
+++ frontend/views/vote/view.php
@@ -6,9 +6,11 @@
  * Time: 下午4:57
  */
 ?>
-
+<style>
+    .content img{margin:0 auto;max-width:95%;display:block;}
+</style>
 <h1 style="text-align:center"><?= $model->name ?></h1>
 <div style="text-align:center"><?= \common\helpers\Html::img($model->cover) ?></div>
-<div class="jumbotron">
+<div class="content">
     <?= \yii\helpers\HtmlPurifier::process($model->description) ?>
 </div>
