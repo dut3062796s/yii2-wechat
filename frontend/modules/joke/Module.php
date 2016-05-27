bc88a03940a444334d18525399bba9bcb28a6a6b
刘俊涛 <liujuntaor@qq.com>
2016-05-27 13:33:52 +0800

    xingzuo

--- frontend/modules/joke/Module.php
+++ frontend/modules/joke/Module.php
@@ -9,6 +9,7 @@
 
 namespace frontend\modules\joke;
 
+use Yii;
 
 class Module extends \yii\base\Module
 {
@@ -16,6 +17,21 @@ class Module extends \yii\base\Module
 
     public function process($name)
     {
-
+        $api = 'http://japi.juhe.cn/joke/content/text.from';
+        
+        if($name == '笑话') {
+            $params = [
+                'key' => 'c722c3da3a13044f0fad2b3b11030e6f',
+                'page' => 1,
+                'pagesize' => 3
+            ];
+            $output = Yii::$app->curl->get($api, $params);
+            $str = '';
+            foreach ($output['result']['data'] as $k => $joke) {
+                $str .= $k . '. ' . $joke['content'] . "\n";
+            }
+            return $str;
+        }
+        return null;
     }
 }
