b9e8ec43fb1598c4c920bbe0d716f7888000968d
刘俊涛 <liujuntaor@qq.com>
2016-05-27 14:10:49 +0800

    vote bug

--- frontend/modules/vote/views/default/_vote.php
+++ frontend/modules/vote/views/default/_vote.php
@@ -10,12 +10,12 @@ use common\helpers\Html;
 <style>.media{margin-top:15px}</style>
 <div class="media">
     <div class="media-left">
-        <a href="<?= \yii\helpers\Url::to(['view', 'id' => $model->id]) ?>">
+        <a href="<?= \yii\helpers\Url::to(['view', 'voteId' => $model->id]) ?>">
             <?= Html::img($model->cover, ['width' => 100, 'height' => 100]) ?>
         </a>
     </div>
     <div class="media-body">
-        <a href="<?= \yii\helpers\Url::to(['view', 'id' => $model->id]) ?>">
+        <a href="<?= \yii\helpers\Url::to(['view', 'voteId' => $model->id]) ?>">
             <h2 class="media-heading"><?= Html::encode($model->name) ?></h2>
             <?= Html::encode($model->description) ?>
             <div>编号:<?= $model->id ?> 票数:<?= $model->num ?></div>
